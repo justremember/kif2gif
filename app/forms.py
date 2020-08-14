309b05e1bcae798efa2c23120e057d694c08bad9
justremember <nothingpersonal.qwerty@gmail.com>
2020-08-14 23:16:45 +0800

    minor form changes

--- app/forms.py
+++ app/forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import DataRequired
 
 
 class KifuForm(FlaskForm):
-    kifu = TextAreaField('Paste kifu here', rows=50, cols=50, validators=[DataRequired()])
-    delay = FloatField('Time delay between moves, in seconds', default=1.0)
-    start = IntegerField('Start position # of the gif', default=0)
-    end = IntegerField('End position # of the gif', default=9999)
+    kifu = TextAreaField('Paste kifu here', validators=[DataRequired()])
+    delay = FloatField('Time delay between moves, in seconds (with millisecond precision)', default=1.0)
+    start = IntegerField('Start move # of the gif', default=0)
+    end = IntegerField('End move # of the gif', default=9999)
     submit = SubmitField('Submit')
