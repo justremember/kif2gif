5c6d732990228488a0f49ef917d7eab09f09c5b7
justremember <nothingpersonal.qwerty@gmail.com>
2020-08-14 23:13:16 +0800

    add .gitkeep to gifs folder so it gets pushed

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired
 
 
 class KifuForm(FlaskForm):
-    kifu = TextAreaField('Paste kifu here', validators=[DataRequired()])
+    kifu = TextAreaField('Paste kifu here', rows=50, cols=50, validators=[DataRequired()])
     delay = FloatField('Time delay between moves, in seconds', default=1.0)
     start = IntegerField('Start position # of the gif', default=0)
     end = IntegerField('End position # of the gif', default=9999)
